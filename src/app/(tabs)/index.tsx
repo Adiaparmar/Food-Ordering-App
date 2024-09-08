d1a1d5f978c50115baf0fe3b03956605f2776de4
adiaparmar <adiaparmar@gmail.com>
2024-09-09 00:04:24 +0530

    code structure for screens

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,5 @@
-import { View, FlatList } from "react-native";
-import products from "@assets/data/products";
-import ProductListItem from "@components/ProductListItem";
+import { Redirect } from "expo-router";
 
-export default function MenuScreen() {
-  return (
-    <View>
-      {/* <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} /> */}
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-        numColumns={2}
-        contentContainerStyle={{ gap: 10, padding: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
-      />
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href={"/menu"} />;
 }
